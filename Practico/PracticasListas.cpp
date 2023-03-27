79587110e93e761834dd302c2e9c1f84a00f5c1c
martincabrera <71331753+martincabrera612@users.noreply.github.com>
2023-03-27 20:32:39 -0300

    Agregue una carpeta de practico donde guardo algunos archivos con codigo de practica

---
+++ Practico/PracticasListas.cpp
@@ -0,0 +1,69 @@
+#include <iostream>;
+using namespace std;
+
+
+struct NodoLista  {
+	int dato;
+	NodoLista* sig;
+};
+
+int main () {
+
+	NodoLista* inicio = new NodoLista;
+	inicio-> dato = 1;
+	inicio-> sig = NULL;
+
+	//Insertar un nuevo elemento
+
+	NodoLista* nuevo = new NodoLista;
+	nuevo-> dato = 2;
+
+	inicio-> sig = nuevo;
+	nuevo-> sig = NULL;
+
+	//Recorrer una lista correctamente 
+
+	NodoLista* aux = inicio;
+
+	while (aux != NULL) {
+		aux = aux ->sig;
+
+	}
+
+
+	
+
+	return 0;
+}
+
+
+
+
+//Ejemplo simple 
+struct NodoLista {
+    int dato;
+    NodoLista* sig;
+};
+
+int main() {
+    
+    NodoLista* inicio = new NodoLista;
+    inicio->dato = 1;
+    inicio->sig = NULL;
+    
+    NodoLista* nuevo = new NodoLista;
+    nuevo-> dato = 2;
+    nuevo->sig = NULL;
+    inicio->sig = nuevo; //Inserto el nuevo elemento.
+    
+    NodoLista* aux = new NodoLista; //Creo un auxiliar para no perder la referencia de donde inicia la lista
+    aux = inicio;
+    while (aux != NULL) { 			//Recorro hasta que termine
+        cout<< aux->dato << endl;   //Muestro los datos que tiene el nodo.
+        aux = aux->sig;				// Avanzo por la lista
+    }
+  
+    cout<< inicio->dato<< endl;  // La lista sigue conteniendo sus datos
+    cout<< nuevo->dato<< endl;
+    cout<< "OK!"<< endl;
+
